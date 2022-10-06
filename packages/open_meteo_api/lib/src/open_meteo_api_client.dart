c38ecbd29a60579c26d9e3444aad32ede691ad14
mykola.zaretskyy <mzaretskyy@oneviewhealthcare.com>
2022-10-06 11:44:26 +0200

    data layer finalization

--- packages/open_meteo_api/lib/src/open_meteo_api_client.dart
+++ packages/open_meteo_api/lib/src/open_meteo_api_client.dart
@@ -1,31 +1,79 @@
+import 'dart:async';
 import 'dart:convert';
 
-import 'package:http/http.dart' as _httpClient;
-import '../open_meteo_api.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_meteo_api/open_meteo_api.dart';
 
-const String _baseUrlGeocoding = 'api.open-meteo.com';
+/// Exception thrown when locationSearch fails.
+class LocationRequestFailure implements Exception {}
 
-Future<Location> locationSearch(String query) async {
-  final locationRequest = Uri.https(
-    _baseUrlGeocoding,
-    '/v1/search',
-    {'name': query, 'count': '1'},
-  );
+/// Exception thrown when the provided location is not found.
+class LocationNotFoundFailure implements Exception {}
 
-  final locationResponse = await _httpClient.get(locationRequest);
-  if (locationResponse.statusCode != 200) {
-    throw Exception('Location request failed');
+/// Exception thrown when getWeather fails.
+class WeatherRequestFailure implements Exception {}
+
+/// Exception thrown when weather for provided location is not found.
+class WeatherNotFoundFailure implements Exception {}
+
+class OpenMeteoApiClient {
+  OpenMeteoApiClient({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  static const String _baseUrlGeocoding = 'geocoding-api.open-meteo.com';
+  static const String _baseUrlWeather = 'api.open-meteo.com';
+
+  final http.Client _httpClient;
+
+  Future<Location> locationSearch(String query) async {
+    final locationRequest = Uri.https(
+      _baseUrlGeocoding,
+      '/v1/search',
+      {'name': query, 'count': '1'},
+    );
+
+    final locationResponse = await _httpClient.get(locationRequest);
+    if (locationResponse.statusCode != 200) {
+      throw Exception('Location request failed');
+    }
+
+    final locationJson = jsonDecode(locationResponse.body) as Map;
+    if (!locationJson.containsKey('results')) {
+      throw Exception('Location not found');
+    }
+
+    final results = locationJson['results'] as List;
+    if (results.isEmpty) {
+      throw Exception('Location not found');
+    }
+
+    return Location.fromJson(results.first as Map<String, dynamic>);
   }
 
-  final locationJson = jsonDecode(locationResponse.body) as Map;
-  if (!locationJson.containsKey('results')) {
-    throw Exception('Location not found');
-  }
+  Future<Weather> getWeather({
+    required double latitude,
+    required double longitude,
+  }) async {
+    final weatherRequest = Uri.https(_baseUrlWeather, 'v1/forecast', {
+      'latitude': '$latitude',
+      'longitude': '$longitude',
+      'current_weather': 'true'
+    });
 
-  final results = locationJson['results'] as List;
-  if (results.isEmpty) {
-    throw Exception('Location not found');
-  }
+    final weatherResponse = await _httpClient.get(weatherRequest);
 
-  return Location.fromJson(results.first as Map<String, dynamic>);
+    if (weatherResponse.statusCode != 200) {
+      throw Exception('Location request failed');
+    }
+
+    final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+
+    if (!bodyJson.containsKey('current_weather')) {
+      throw Exception('Location not found');
+    }
+
+    final weatherJson = bodyJson['current_weather'] as Map<String, dynamic>;
+
+    return Weather.fromJson(weatherJson);
+  }
 }
